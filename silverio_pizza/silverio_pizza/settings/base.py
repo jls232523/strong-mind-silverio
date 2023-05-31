deda7254fd0ff61e022dfe0d17cb0009b5b4b2f0
jls232523 <33877502+jls232523@users.noreply.github.com>
2023-05-31 15:23:17 -0700

    ui
    Signed-off-by: jls232523 <joshualee2@me.com>

--- silverio_pizza/silverio_pizza/settings/base.py
+++ silverio_pizza/silverio_pizza/settings/base.py
@@ -133,6 +133,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'staticfiles')# Add additional directories if needed
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 
 
 
